41f7c23d4b4234814795cad85fe9f85b7e42337a
jsofii <sofi-0106@hotmail.com>
2018-08-11 15:21:24 -0500

    prueba2

--- WindowsFormsApplication1/WindowsFormsApplication1/ModuloReportesEstadisticos/FrmReporteViajes.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloReportesEstadisticos/FrmReporteViajes.Designer.cs
@@ -69,8 +69,10 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(800, 738);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1067, 908);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
@@ -83,14 +85,15 @@
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.groupBox5, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 20);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 26);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.821211F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.7947F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.62515F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.758939F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 715);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1059, 878);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel3
@@ -101,11 +104,12 @@
             this.tableLayoutPanel3.Controls.Add(this.buttonGuardar, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.buttonCancelar, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 683);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 838);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(788, 29);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1051, 36);
             this.tableLayoutPanel3.TabIndex = 3;
             // 
             // buttonGuardar
@@ -115,9 +119,10 @@
             this.buttonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonGuardar.Image = ((System.Drawing.Image)(resources.GetObject("buttonGuardar.Image")));
             this.buttonGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonGuardar.Location = new System.Drawing.Point(126, 3);
+            this.buttonGuardar.Location = new System.Drawing.Point(148, 4);
+            this.buttonGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonGuardar.Name = "buttonGuardar";
-            this.buttonGuardar.Size = new System.Drawing.Size(142, 23);
+            this.buttonGuardar.Size = new System.Drawing.Size(229, 28);
             this.buttonGuardar.TabIndex = 4;
             this.buttonGuardar.Text = "Nuevo Reporte";
             this.buttonGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -129,9 +134,10 @@
             this.buttonCancelar.AutoSize = true;
             this.buttonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCancelar.Image = ((System.Drawing.Image)(resources.GetObject("buttonCancelar.Image")));
-            this.buttonCancelar.Location = new System.Drawing.Point(531, 3);
+            this.buttonCancelar.Location = new System.Drawing.Point(708, 4);
+            this.buttonCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonCancelar.Name = "buttonCancelar";
-            this.buttonCancelar.Size = new System.Drawing.Size(119, 23);
+            this.buttonCancelar.Size = new System.Drawing.Size(159, 28);
             this.buttonCancelar.TabIndex = 3;
             this.buttonCancelar.Text = "Cancelar";
             this.buttonCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -142,9 +148,11 @@
             this.groupBox2.Controls.Add(this.tableLayoutPanel2);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(3, 37);
+            this.groupBox2.Location = new System.Drawing.Point(4, 46);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(788, 164);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(1051, 200);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Parámetros ";
@@ -161,21 +169,23 @@
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel5, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnGenerarReporte, 1, 2);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 20);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 26);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.94805F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.05195F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(782, 141);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1043, 170);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // checkBoxFecha
             // 
             this.checkBoxFecha.AutoSize = true;
-            this.checkBoxFecha.Location = new System.Drawing.Point(4, 56);
+            this.checkBoxFecha.Location = new System.Drawing.Point(5, 68);
+            this.checkBoxFecha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxFecha.Name = "checkBoxFecha";
-            this.checkBoxFecha.Size = new System.Drawing.Size(120, 22);
+            this.checkBoxFecha.Size = new System.Drawing.Size(152, 28);
             this.checkBoxFecha.TabIndex = 1;
             this.checkBoxFecha.Text = "Rango Fecha:";
             this.checkBoxFecha.UseVisualStyleBackColor = true;
@@ -184,9 +194,10 @@
             // checkBoxSolicitante
             // 
             this.checkBoxSolicitante.AutoSize = true;
-            this.checkBoxSolicitante.Location = new System.Drawing.Point(4, 4);
+            this.checkBoxSolicitante.Location = new System.Drawing.Point(5, 5);
+            this.checkBoxSolicitante.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxSolicitante.Name = "checkBoxSolicitante";
-            this.checkBoxSolicitante.Size = new System.Drawing.Size(99, 22);
+            this.checkBoxSolicitante.Size = new System.Drawing.Size(122, 28);
             this.checkBoxSolicitante.TabIndex = 2;
             this.checkBoxSolicitante.Text = "Solicitante:";
             this.checkBoxSolicitante.UseVisualStyleBackColor = true;
@@ -200,20 +211,23 @@
             this.tableLayoutPanel4.Controls.Add(this.groupBox3, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.groupBox4, 1, 0);
             this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel4.Location = new System.Drawing.Point(204, 56);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(272, 68);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(574, 42);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(766, 49);
             this.tableLayoutPanel4.TabIndex = 4;
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dateTimePickerDesde);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Location = new System.Drawing.Point(4, 4);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(281, 36);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(375, 41);
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Desde:";
@@ -222,18 +236,21 @@
             // 
             this.dateTimePickerDesde.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dateTimePickerDesde.Enabled = false;
-            this.dateTimePickerDesde.Location = new System.Drawing.Point(3, 20);
+            this.dateTimePickerDesde.Location = new System.Drawing.Point(4, 26);
+            this.dateTimePickerDesde.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePickerDesde.Name = "dateTimePickerDesde";
-            this.dateTimePickerDesde.Size = new System.Drawing.Size(275, 24);
+            this.dateTimePickerDesde.Size = new System.Drawing.Size(367, 29);
             this.dateTimePickerDesde.TabIndex = 0;
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.dateTimePickerHasta);
             this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox4.Location = new System.Drawing.Point(290, 3);
+            this.groupBox4.Location = new System.Drawing.Point(387, 4);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(281, 36);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Size = new System.Drawing.Size(375, 41);
             this.groupBox4.TabIndex = 1;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Hasta:";
@@ -242,9 +259,10 @@
             // 
             this.dateTimePickerHasta.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dateTimePickerHasta.Enabled = false;
-            this.dateTimePickerHasta.Location = new System.Drawing.Point(3, 20);
+            this.dateTimePickerHasta.Location = new System.Drawing.Point(4, 26);
+            this.dateTimePickerHasta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePickerHasta.Name = "dateTimePickerHasta";
-            this.dateTimePickerHasta.Size = new System.Drawing.Size(275, 24);
+            this.dateTimePickerHasta.Size = new System.Drawing.Size(367, 29);
             this.dateTimePickerHasta.TabIndex = 0;
             // 
             // tableLayoutPanel5
@@ -254,11 +272,12 @@
             this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel5.Controls.Add(this.comboBoxSolicitantes, 0, 0);
             this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel5.Location = new System.Drawing.Point(204, 4);
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(272, 5);
+            this.tableLayoutPanel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel5.Name = "tableLayoutPanel5";
             this.tableLayoutPanel5.RowCount = 1;
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel5.Size = new System.Drawing.Size(574, 45);
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(766, 54);
             this.tableLayoutPanel5.TabIndex = 5;
             // 
             // comboBoxSolicitantes
@@ -266,18 +285,20 @@
             this.comboBoxSolicitantes.Dock = System.Windows.Forms.DockStyle.Fill;
             this.comboBoxSolicitantes.Enabled = false;
             this.comboBoxSolicitantes.FormattingEnabled = true;
-            this.comboBoxSolicitantes.Location = new System.Drawing.Point(3, 3);
+            this.comboBoxSolicitantes.Location = new System.Drawing.Point(4, 4);
+            this.comboBoxSolicitantes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxSolicitantes.Name = "comboBoxSolicitantes";
-            this.comboBoxSolicitantes.Size = new System.Drawing.Size(281, 26);
+            this.comboBoxSolicitantes.Size = new System.Drawing.Size(375, 32);
             this.comboBoxSolicitantes.TabIndex = 0;
             // 
             // btnGenerarReporte
             // 
             this.btnGenerarReporte.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnGenerarReporte.Image = ((System.Drawing.Image)(resources.GetObject("btnGenerarReporte.Image")));
-            this.btnGenerarReporte.Location = new System.Drawing.Point(415, 105);
+            this.btnGenerarReporte.Location = new System.Drawing.Point(553, 126);
+            this.btnGenerarReporte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGenerarReporte.Name = "btnGenerarReporte";
-            this.btnGenerarReporte.Size = new System.Drawing.Size(152, 32);
+            this.btnGenerarReporte.Size = new System.Drawing.Size(203, 39);
             this.btnGenerarReporte.TabIndex = 6;
             this.btnGenerarReporte.Text = "Generar Reporte";
             this.btnGenerarReporte.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -289,9 +310,10 @@
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(788, 24);
+            this.label1.Size = new System.Drawing.Size(1051, 29);
             this.label1.TabIndex = 5;
             this.label1.Text = "REPORTE DE VIAJES";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -300,9 +322,11 @@
             // 
             this.groupBox5.Controls.Add(this.panel1);
             this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox5.Location = new System.Drawing.Point(3, 207);
+            this.groupBox5.Location = new System.Drawing.Point(4, 254);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(788, 470);
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox5.Size = new System.Drawing.Size(1051, 576);
             this.groupBox5.TabIndex = 6;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Reporte Generado:";
@@ -311,9 +335,10 @@
             // 
             this.panel1.Controls.Add(this.reportViewer1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 20);
+            this.panel1.Location = new System.Drawing.Point(4, 26);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(782, 447);
+            this.panel1.Size = new System.Drawing.Size(1043, 546);
             this.panel1.TabIndex = 0;
             // 
             // reportViewer1
@@ -322,17 +347,19 @@
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApplication1.ModuloReportesEstadisticos.ReporteHistorialKilometraje.r" +
     "dlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(782, 447);
+            this.reportViewer1.Size = new System.Drawing.Size(1043, 546);
             this.reportViewer1.TabIndex = 0;
             // 
             // FrmReporteViajes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 738);
+            this.ClientSize = new System.Drawing.Size(1067, 908);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmReporteViajes";
             this.Text = "FrmReporteViajes";
             this.groupBox1.ResumeLayout(false);
