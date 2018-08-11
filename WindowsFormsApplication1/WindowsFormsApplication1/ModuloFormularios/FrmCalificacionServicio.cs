ea54f03bcfa28079cad0d53a6b72eebe426e93d6
Cristian <cristianbb98@gmail.com>
2018-08-11 09:21:09 -0500

    Interfaz de navegacion lista

--- WindowsFormsApplication1/WindowsFormsApplication1/ModuloFormularios/FrmCalificacionServicio.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloFormularios/FrmCalificacionServicio.cs
@@ -15,46 +15,74 @@ namespace ModuloFormularios
     {
         private CSCalificacionDeServicio calificacionServicio;
 
-        public FrmCalificacionServicio()
+        public FrmCalificacionServicio(string idUsuario, string [] reservas)
         {
             InitializeComponent();
             calificacionServicio = new CSCalificacionDeServicio();
+            crearCombo(reservas);
+            txt_nomConductor.Text= "JAPAN";
+        }
+
+        public void crearCombo(string[] reservas)
+        {
+            foreach (string reserva in reservas)
+            {
+                cb_reservas.Items.Add(reserva);
+                cb_reservas.SelectedItem = 2;
+            }
         }
 
         private void obtenerDatos(object sender, EventArgs e)
         {
-            if (rb_puntSalidaT.Checked)
+            if (cb_reservas.SelectedItem != null)
             {
-                calificacionServicio.setPuntualSalida("SI");
-            }
-            else {
-                calificacionServicio.setPuntualSalida("NO");
-            }
+                calificacionServicio.setIdReserva(cb_reservas.SelectedItem+"");
+                if (rb_puntSalidaT.Checked)
+                {
+                    calificacionServicio.setPuntualSalida("SI");
+                }
+                else
+                {
+                    calificacionServicio.setPuntualSalida("NO");
+                }
 
-            if (rb_limpiezaT.Checked)
-            {
-                calificacionServicio.setLimpiezaVehiculo("SI");
-            }
-            else
-            {
-                calificacionServicio.setLimpiezaVehiculo("NO");
-            }
-            if (rb_puntLlegadaT.Checked) {
-                calificacionServicio.setPuntualAlDestino("SI");
-            } else {
-                calificacionServicio.setPuntualAlDestino("NO");
-            }
+                if (rb_limpiezaT.Checked)
+                {
+                    calificacionServicio.setLimpiezaVehiculo("SI");
+                }
+                else
+                {
+                    calificacionServicio.setLimpiezaVehiculo("NO");
+                }
+                if (rb_puntLlegadaT.Checked)
+                {
+                    calificacionServicio.setPuntualAlDestino("SI");
+                }
+                else
+                {
+                    calificacionServicio.setPuntualAlDestino("NO");
+                }
 
-            if (rb_puntRetornoT.Checked)
-{
-                calificacionServicio.setPuntualAlRetorno("SI");
+                if (rb_puntRetornoT.Checked)
+                {
+                    calificacionServicio.setPuntualAlRetorno("SI");
+                }
+                else
+                {
+                    calificacionServicio.setPuntualAlRetorno("NO");
+                }
+                if (!txt_comenAdicionales.Text.Equals(""))
+                {
+                    calificacionServicio.setComentariosAdicionales(txt_comenAdicionales.Text);
+                }
+                //MessageBox.Show(calificacionServicio.getPuntualAlDestino() + " " + calificacionServicio.getPuntualAlRetorno() + " " +
+                //                    calificacionServicio.getLimpiezaVehiculo() + " " + calificacionServicio.getPuntualSalida() + calificacionServicio.getComentariosAdicionales()
+                //    + " " + txt_nomConductor.Text + " " + cb_reservas.SelectedItem);
+
             }
-            else {
-                calificacionServicio.setPuntualAlRetorno("NO");
-            }
-            if (!txt_comenAdicionales.Text.Equals(""))
-            {
-                calificacionServicio.setComentariosAdicionales(txt_comenAdicionales.Text);
+            else{ 
+                MessageBox.Show("ERROR AL INGRESAR LOS DATOS");
+
             }
         }
     }
--- WindowsFormsApplication1/WindowsFormsApplication1/ModuloFormularios/FrmPrincipalFormularioProfesor.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ModuloFormularios/FrmPrincipalFormularioProfesor.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,9 +15,12 @@ namespace WindowsFormsApplication1.ModuloFormularios
     public partial class FrmPrincipalFormularioProfesor : Form
     {
         Usuario usu;
+        private Conexion cnx;
+        private SqlConnection conn;
         public FrmPrincipalFormularioProfesor()
         {
             InitializeComponent();
+            this.cnx = new Conexion();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -49,15 +53,16 @@ namespace WindowsFormsApplication1.ModuloFormularios
 
         private void button2_Click(object sender, EventArgs e)
         {
-            FrmCalificacionServicio calificacionServicio = new FrmCalificacionServicio();
+          if (existeCalificacion()) { 
+            FrmCalificacionServicio calificacionServicio = new FrmCalificacionServicio(usu.IDusuario1+"",idReservaCalificacion());
             calificacionServicio.Show();
+        }else{
+                FrmMensajeError error = new FrmMensajeError();
+                error.Show();
+        
         }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
         }
-
+        
         private void FrmPrincipalFormularioProfesor_Load(object sender, EventArgs e)
         {
 
@@ -71,6 +76,66 @@ namespace WindowsFormsApplication1.ModuloFormularios
         private void btnMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        public string[] idReservaCalificacion()
+        {
+            string sql = "select idReservaAprob from reservaaprobada where idusuario=" + usu.IDusuario1 + " and calificacionservicio is null and estadosolicitud is null ";
+            string[] valores = null;
+            conn = new SqlConnection(cnx.stringConexion);
+            try
+            {
+                conn.Open();
+                SqlCommand comando = new SqlCommand(sql, conn);
+                SqlDataReader reader = comando.ExecuteReader();
+                int i = 0;
+                while (reader.Read())
+                {
+
+                    valores[i] = reader[0] + "";
+                    i++;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return valores;
+        }
+
+        public bool existeCalificacion()
+        {
+            string sql = "select count(*) from reservaaprobada where idusuario=" + usu.IDusuario1 + " and calificacionservicio is null and estadosolicitud is null";
+            string valor = "";
+            MessageBox.Show(sql + "");
+            conn = new SqlConnection(cnx.stringConexion);
+            try
+            {
+                conn.Open();
+                SqlCommand comando = new SqlCommand(sql, conn);
+                SqlDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    valor = reader[0] + "";
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+
+            if (valor == "0")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
